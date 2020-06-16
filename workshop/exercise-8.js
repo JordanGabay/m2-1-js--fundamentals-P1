6f1cfa865473ae0da4445fe4d7adb7ee57a3786d
Jordan Gabay <gabay.jordan@outlook.com>
2020-06-15 22:04:59 -0400

    Exercise 8 Complete

--- workshop/exercise-8.js
+++ workshop/exercise-8.js
@@ -3,3 +3,11 @@
 // between 0 and 5000
 
 // ANSWER: the number you should see in the console is 3127500
+
+let x = 0;
+for (let num = 0; num <= 5000; num++) {
+    if (num % 4 === 0) {
+        x += num;
+    }
+}
+console.log(`The sum is ${x}`);
