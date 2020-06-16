43145fca61cd242379c2ec7d2efc833b4e064ff3
Jordan Gabay <gabay.jordan@outlook.com>
2020-06-15 21:17:19 -0400

    Exercis 6.3 Complete

--- workshop/exercise-6.3.js
+++ workshop/exercise-6.3.js
@@ -1,2 +1,11 @@
 // Q6.3
 // Write a loop that will print to the console all of the EVEN integers from 1 to 100.
+
+
+for (let x = 0; x <= 100; x++){
+    if (x!==0) {
+        if (x % 2 === 0){
+    console.log(x)
+        }
+    }
+}
